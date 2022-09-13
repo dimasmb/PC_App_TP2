5fd4d3f11e72293831bb9fd22af3537514728da5
Dimas <dbosch@itba.edu.ar>
2022-09-13 19:06:34 -0300

    Falta solo la parte de orientación

--- Station/src/mainwindow.py
+++ Station/src/mainwindow.py
@@ -1,5 +1,6 @@
 # PyQt5 modules
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QTimer
 
 # Project modules
 from src.ui.mainwindow import Ui_Stations
@@ -7,6 +8,7 @@ from src.ui.mainwindow import Ui_Stations
 # Utilities
 from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
 from matplotlib.figure import Figure
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as np
 
 class Stations(QMainWindow, Ui_Stations):
@@ -26,85 +28,107 @@ class Stations(QMainWindow, Ui_Stations):
             self.orientaciones[i].setText('0º')
 
         #inicializo los graficos
-        x = np.arange(-2.0, 2.0, 0.5)
-        y = np.arange(-4.0, 4.0, 0.5)
-        X, Y = np.meshgrid(x, y)
-        zs = np.zeros(X.shape)
-        self.figure_station0 = Figure()
+
+        self.x_init = np.array([-2, -2, 2, 2])
+        self.y_init = np.array([-4, 4, 4, -4])
+        self.z_init = np.array([0, 0, 0, 0])
+
+        # self.x_init = np.linspace(-2.0, 2.0, 10)
+        # self.y_init = np.linspace(-4.0, 4.0, 10)
+        # X, Y = np.meshgrid(self.x_init, self.y_init)
+        # zs = np.zeros(X.shape)
+
+        self.figure_station0 = Figure(figsize=(3, 3))
         self.canvas_station0 = FigureCanvas(self.figure_station0)
         self.Imagen_estacion0.addWidget(self.canvas_station0)
         self.ax_station0 = self.figure_station0.add_subplot(projection='3d')
-        self.ax_station0.quiver([0, 0], [-4, -4], [0, 0], [-1, 0], [0, 0], [0, 1], length=2, color ='red', normalize=True)
-        self.ax_station0.plot_surface(X, Y, zs)
-        self.ax_station0.set_xlim(-4, 4)
-        self.ax_station0.set_ylim(-4, 4)
-        self.ax_station0.set_zlim(-4, 4)
-        # self.ax_station0.set_axis_off()
-        self.ax_station0.grid(True)
-        self.ax_station0.set_xticklabels([])
-        self.ax_station0.set_yticklabels([])
-        self.ax_station0.set_zticklabels([])
-        self.ax_station0.view_init(30, -45)
-        self.canvas_station0.show()
 
         self.figure_station1 = Figure(figsize=(3, 3))
         self.canvas_station1 = FigureCanvas(self.figure_station1)
         self.Imagen_estacion1.addWidget(self.canvas_station1)
         self.ax_station1 = self.figure_station1.add_subplot(projection='3d')
-        self.ax_station1.quiver([-3, -3, -3], [-3, -3, -3], [3, 3, 3], [1, 0, 0], [0, -1, 0], [0, 0, 1], length=2, color ='red', normalize=True)
-        self.ax_station1.plot_surface(X, Y, zs)
-        self.ax_station1.set_xlim(-4, 4)
-        self.ax_station1.set_ylim(-4, 4)
-        self.ax_station1.set_zlim(-4, 4)
-        self.ax_station1.set_axis_off()
-        self.canvas_station1.show()
 
         self.figure_station2 = Figure(figsize=(3, 3))
         self.canvas_station2 = FigureCanvas(self.figure_station2)
         self.Imagen_estacion2.addWidget(self.canvas_station2)
         self.ax_station2 = self.figure_station2.add_subplot(projection='3d')
-        self.ax_station2.quiver([-3, -3, -3], [-3, -3, -3], [3, 3, 3], [1, 0, 0], [0, -1, 0], [0, 0, 1], length=2, color ='red', normalize=True)
-        self.ax_station2.plot_surface(X, Y, zs)
-        self.ax_station2.set_xlim(-4, 4)
-        self.ax_station2.set_ylim(-4, 4)
-        self.ax_station2.set_zlim(-4, 4)
-        self.ax_station2.set_axis_off()
-        self.figure_station2.tight_layout()
-        self.canvas_station2.show()
 
         self.figure_station3 = Figure(figsize=(3, 3))
         self.canvas_station3 = FigureCanvas(self.figure_station3)
         self.Imagen_estacion3.addWidget(self.canvas_station3)
         self.ax_station3 = self.figure_station3.add_subplot(projection='3d')
-        self.ax_station3.quiver([-3, -3, -3], [-3, -3, -3], [3, 3, 3], [1, 0, 0], [0, -1, 0], [0, 0, 1], length=2, color ='red', normalize=True)
-        self.ax_station3.plot_surface(X, Y, zs)
-        self.ax_station3.set_xlim(-4, 4)
-        self.ax_station3.set_ylim(-4, 4)
-        self.ax_station3.set_zlim(-4, 4)
-        self.ax_station3.set_axis_off()
-        self.figure_station3.tight_layout()
-        self.canvas_station3.show()
 
         self.figure_station4 = Figure(figsize=(3, 3))
         self.canvas_station4 = FigureCanvas(self.figure_station4)
         self.Imagen_estacion4.addWidget(self.canvas_station4)
         self.ax_station4 = self.figure_station4.add_subplot(projection='3d')
-        self.ax_station4.quiver([-3, -3, -3], [-3, -3, -3], [3, 3, 3], [1, 0, 0], [0, -1, 0], [0, 0, 1], length=2, color ='red', normalize=True)
-        self.ax_station4.plot_surface(X, Y, zs)
-        self.ax_station4.set_xlim(-4, 4)
-        self.ax_station4.set_ylim(-4, 4)
-        self.ax_station4.set_zlim(-4, 4)
-        self.ax_station4.set_axis_off()
-        self.figure_station4.tight_layout()
-        self.canvas_station4.show()
 
+        self.figures = [self.figure_station0, self.figure_station1, self.figure_station2, self.figure_station3, self.figure_station4]
         self.axes = [self.ax_station0, self.ax_station1, self.ax_station2, self.ax_station3, self.ax_station4]
         self.canvases = [self.canvas_station0, self.canvas_station1, self.canvas_station2, self.canvas_station3, self.canvas_station4]
 
+        vertices = [list(zip(self.x_init, self.y_init, self.z_init))]
+        for i in range(len(self.axes)):
+            self.axes[i].quiver([0, 0], [-4, -4], [0, 0], [-1, 0], [0, 0], [0, 1], length=2, color='red', normalize=True)
+            self.axes[i].add_collection3d(Poly3DCollection(vertices))
+            self.axes[i].set_xlim(-4, 4)
+            self.axes[i].set_ylim(-4, 4)
+            self.axes[i].set_zlim(-4, 4)
+            self.axes[i].grid(True)
+            self.axes[i].set_xticks([])
+            self.axes[i].set_yticks([])
+            self.axes[i].set_zticks([])
+            self.figures[i].tight_layout()
+            self.canvases[i].show()
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.refreshGrafik)
+        self.timer.start(50)
+
+        self.cont=0
 
     def refreshGrafik(self):
+        self.cont+=1
+
+        self.rolidos[0].setText(str(self.cont)+'º')
+        self.orientaciones[1].setText(str(self.cont)+'º')
+        self.rolidos[2].setText(str(self.cont)+'º')
+        self.rolidos[3].setText(str(self.cont)+'º')
+        # self.cabeceos[0].setText(str(self.cont)+'º')
         rolido_val = [int(i.text()[:-1]) for i in self.rolidos]
         cabeceo_val = [int(i.text()[:-1]) for i in self.cabeceos]
         orient_val = [int(i.text()[:-1]) for i in self.orientaciones]
+
+        for i in range(len(self.axes)):
+            xx = self.x_init * np.cos(rolido_val[i]*(np.pi/180))
+            yy = self.y_init* np.cos(cabeceo_val[i]*(np.pi/180))
+            zz = self.z_init + self.x_init * np.sin(rolido_val[i]*(np.pi/180)) + self.y_init * np.sin(cabeceo_val[i]*(np.pi/180))
+
+            # xx[0] += self.x_init[0] * np.sin(orient_val[i])
+            # xx[1] -= self.x_init[1] * np.sin(orient_val[i])
+            # xx[2] -= self.x_init[2] * np.sin(orient_val[i])
+            # xx[3] += self.x_init[3] * np.sin(orient_val[i])
+            #
+            # yy[0] -= self.y_init[0] * (1 - np.sin(orient_val[i]))
+            # yy[1] -= self.y_init[1] * (1 - np.sin(orient_val[i]))
+            # yy[2] += self.y_init[2] * (1 - np.sin(orient_val[i]))
+            # yy[3] += self.y_init[3] * (1 - np.sin(orient_val[i]))
+            # yy += self.y_init * np.cos(orient_val[i])
+
+            vertices = [list(zip(xx, yy, zz))]
+
+
+            self.axes[i].cla()
+            self.axes[i].quiver([0, 0], [-4, -4], [0, 0], [-1, 0], [0, 0], [0, 1], length=2, color='red', normalize=True)
+            self.axes[i].add_collection3d(Poly3DCollection(vertices))
+            self.axes[i].set_xlim(-4, 4)
+            self.axes[i].set_ylim(-4, 4)
+            self.axes[i].set_zlim(-4, 4)
+            self.axes[i].grid(True)
+            self.axes[i].set_xticks([])
+            self.axes[i].set_yticks([])
+            self.axes[i].set_zticks([])
+            self.figures[i].tight_layout()
+            self.canvases[i].draw()
 
 
