1181aaedccaa00a7683421a69725c142f013682a
Dimas <dbosch@itba.edu.ar>
2022-09-14 01:54:59 -0300

    SIgue faltando lo de orientación

--- Station/src/mainwindow.py
+++ Station/src/mainwindow.py
@@ -33,6 +33,11 @@ class Stations(QMainWindow, Ui_Stations):
         self.y_init = np.array([-4, 4, 4, -4])
         self.z_init = np.array([0, 0, 0, 0])
 
+        self.vert0_init = np.arctan(2)*(180/np.pi)
+        self.vert1_init = 180 - self.vert0_init
+        self.vert2_init = -self.vert1_init
+        self.vert3_init = -self.vert0_init
+        self.vert_init = [self.vert0_init, self.vert1_init, self.vert2_init, self.vert3_init, ]
         # self.x_init = np.linspace(-2.0, 2.0, 10)
         # self.y_init = np.linspace(-4.0, 4.0, 10)
         # X, Y = np.meshgrid(self.x_init, self.y_init)
@@ -89,48 +94,33 @@ class Stations(QMainWindow, Ui_Stations):
         self.cont=0
 
     def refreshGrafik(self):
-        self.cont=1
+        self.cont+=1
 
         self.rolidos[0].setText(str(self.cont)+'º')
-        self.orientaciones[1].setText(str(self.cont)+'º')
-        self.rolidos[2].setText(str(self.cont)+'º')
-        self.rolidos[3].setText(str(self.cont)+'º')
-        # self.cabeceos[0].setText(str(self.cont)+'º')
+        # self.orientaciones[1].setText(str(self.cont)+'º')
+        self.cabeceos[2].setText(str(self.cont)+'º')
+
         rolido_val = [int(i.text()[:-1]) for i in self.rolidos]
         cabeceo_val = [int(i.text()[:-1]) for i in self.cabeceos]
         orient_val = [int(i.text()[:-1]) for i in self.orientaciones]
 
+        # x_val=np.cos(orient_val)
+        # y_val = np.sin(orient_val)
+        # z_val=np.sin(cabeceo_val)
         for i in range(len(self.axes)):
             xx = self.x_init * np.cos(rolido_val[i]*(np.pi/180))
-            yy = self.y_init* np.cos(cabeceo_val[i]*(np.pi/180))
-            zz = self.z_init + self.x_init * np.sin(rolido_val[i]*(np.pi/180)) + self.y_init * np.sin(cabeceo_val[i]*(np.pi/180))
+            yy = self.y_init * np.cos(cabeceo_val[i]*(np.pi/180))
+            zz = self.x_init * np.sin(rolido_val[i]*(np.pi/180)) + self.y_init * np.sin(cabeceo_val[i]*(np.pi/180))
 
-            if(orient_val[i]>=0 and orient_val[i]<=45):
-                xx[0] -= self.x_init[0] * np.sin(orient_val[i]*(np.pi/180))
-                xx[1] += self.x_init[1] * np.sin(orient_val[i]*(np.pi/180))
-                xx[2] -= self.x_init[2] * np.sin(orient_val[i]*(np.pi/180))
-                xx[3] += self.x_init[3] * np.sin(orient_val[i]*(np.pi/180))
 
-                yy[0] += self.y_init[0] * (1 - np.cos(orient_val[i]*(np.pi/180)))
-                yy[1] -= self.y_init[1] * (1 - np.cos(orient_val[i]*(np.pi/180)))
-                yy[2] += self.y_init[2] * (1 - np.cos(orient_val[i]*(np.pi/180)))
-                yy[3] -= self.y_init[3] * (1 - np.cos(orient_val[i]*(np.pi/180)))
-            elif(orient_val[i]<=90):
-                xx[0] += self.x_init[0] * np.sin(orient_val[i] * (np.pi / 180))
-                xx[1] -= self.x_init[1] * np.sin(orient_val[i] * (np.pi / 180))
-                xx[2] += self.x_init[2] * np.sin(orient_val[i] * (np.pi / 180))
-                xx[3] += self.x_init[3] * np.sin(orient_val[i] * (np.pi / 180))
+            # xx += np.sqrt(20) * np.sin((np.arctan(xx/yy) + (orient_val[i] * np.ones(4))) * (np.pi / 180))
+            # yy += np.sqrt(20) * np.cos((np.arctan(xx/yy) + (orient_val[i] * np.ones(4))) * (np.pi / 180))
 
-                yy[0] += self.y_init[0] * (1 - np.cos(orient_val[i] * (np.pi / 180)))
-                yy[1] += self.y_init[1] * (1 - np.cos(orient_val[i] * (np.pi / 180)))
-                yy[2] -= self.y_init[2] * (1 - np.cos(orient_val[i] * (np.pi / 180)))
-                yy[3] -= self.y_init[3] * (1 - np.cos(orient_val[i] * (np.pi / 180)))
 
             vertices = [list(zip(xx, yy, zz))]
 
-
             self.axes[i].cla()
-            self.axes[i].quiver([0, 0], [-4, -4], [0, 0], [-1, 0], [0, 0], [0, 1], length=2, color='red', normalize=True)
+            self.axes[i].quiver([(xx[2]+xx[3])/2], [(yy[2]+yy[3])/2], [(zz[2]+zz[3])/2], [0], [0], [1], length=2, color='red', normalize=True)
             self.axes[i].add_collection3d(Poly3DCollection(vertices))
             self.axes[i].set_xlim(-4, 4)
             self.axes[i].set_ylim(-4, 4)
@@ -139,7 +129,9 @@ class Stations(QMainWindow, Ui_Stations):
             # self.axes[i].set_xticks([])
             # self.axes[i].set_yticks([])
             # self.axes[i].set_zticks([])
+
+            self.axes[i].set_xlabel('x')
+            self.axes[i].set_ylabel('y')
+            self.axes[i].set_zlabel('z')
             self.figures[i].tight_layout()
             self.canvases[i].draw()
-
-
